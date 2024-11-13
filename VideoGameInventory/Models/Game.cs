04d207bafa8d599216b2aa72f8d643d32f447e32
igl-myrick <igl.myrick@gmail.com>
2024-11-13 15:47:18 -0800

    add Game model with id, name, and console properties

---
+++ VideoGameInventory/Models/Game.cs
@@ -0,0 +1,9 @@
+namespace VideoGameInventory.Models
+{
+  public class Game
+  {
+    public int GameId { get; set; }
+    public string Name { get; set; }
+    public string Console { get; set; }
+  }
+}
